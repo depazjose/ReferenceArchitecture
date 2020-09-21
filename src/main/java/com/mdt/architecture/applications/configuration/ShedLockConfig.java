5d91e3b876553fa2af8f27e5679ab9897adb25a8
José de Paz <josedepaz@MacBook-Pro-de-Jose.local>
2020-09-20 23:22:30 -0500

    refactoring

--- src/main/java/com/mdt/architecture/applications/configuration/ShedLockConfig.java
+++ src/main/java/com/mdt/architecture/applications/configuration/ShedLockConfig.java
@@ -2,7 +2,6 @@ package com.mdt.architecture.applications.configuration;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
