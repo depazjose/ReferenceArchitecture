4538cdcab4a41c3381aeaad76446daa7216a7081
Manuel Riaño <manuel.riano@globant.com>
2020-12-29 16:08:12 -0500

    use case fixed

--- src/main/java/com/mdt/architecture/domain/usescase/publishinghouse/PublishingHouseUseCaseImpl.java
+++ src/main/java/com/mdt/architecture/domain/usescase/publishinghouse/PublishingHouseUseCaseImpl.java
@@ -1,14 +1,21 @@
 package com.mdt.architecture.domain.usescase.publishinghouse;
 
 import com.mdt.architecture.domain.model.publishingHouse.PublishingHouse;
+import com.mdt.architecture.domain.model.publishingHouse.gateway.PublishingHouseRepository;
 import java.util.List;
 
 public class PublishingHouseUseCaseImpl implements PublishingHouseUseCase {
 
+  private final PublishingHouseRepository publishingHouseRepository;
+
+  public PublishingHouseUseCaseImpl(
+      PublishingHouseRepository publishingHouseRepository) {
+    this.publishingHouseRepository = publishingHouseRepository;
+  }
+
   @Override
   public PublishingHouse savePublishingHouse(PublishingHouse publishingHouse) {
-    System.out.println("saving publishing house if it doesnt exits");
-    return null;
+    return publishingHouseRepository.savePublishingHouse(publishingHouse);
   }
 
   @Override
